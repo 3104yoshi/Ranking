0d37b4f6e055d1e0e39b573cd724aae1cd2c7247
3104yoshi <3104adgjmp@gmail.com>
2023-07-26 01:28:54 +0900

    add function of logout

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import datetime
 import os
 import sqlite3
 from flask import Flask, app, render_template, request, redirect, url_for
-from flask_login import LoginManager, login_required, login_user
+from flask_login import LoginManager, login_required, login_user, logout_user
 
 from flask_login import UserMixin
 
@@ -16,7 +16,6 @@ class User(UserMixin):
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('FLASK_SECRET_KEY')
-print(app.config['SECRET_KEY'])
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -61,6 +60,12 @@ def canSignup(canSignup):
     if canSignup:
         return render_template('canSignup.html', msg="登録が完了しました。")
     return render_template('canSignup.html', msg="入力された ユーザー名 は既に使われています")
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return render_template('login.html')
 
 @app.route('/top', methods=['GET', 'POST'])
 @login_required
